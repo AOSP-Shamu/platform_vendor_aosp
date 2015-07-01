8738b24aa4e5fd046ede4e47e1dc671c301dbbde
Kryten2k35 <Kryten2k35@gmail.com>
2015-07-01 22:53:33 +0100

    Added fmas properties

--- config/common.mk
+++ config/common.mk
@@ -4,6 +4,20 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/aosp/overlay/common
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    fmas.spkr_6ch=35,20,110 \
+    fmas.spkr_2ch=35,25 \
+    fmas.spkr_angles=10 \
+    fmas.spkr_sgain=0 \
+    media.aac_51_output_enabled=true \
+    persist.rcs.supported=0 \
+    persist.radio.sib16_support=1 \
+    drm.service.enabled=true \
+    persist.audio.dualmic.config=endfire \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=false 
+
 PRODUCT_PACKAGES += \
     OTAUpdates
 
